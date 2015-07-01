1ae26f4c92d23457f4a08ca4728de6c4807d5739
Ian Bevel & Jeff Lalic <student@epicodus.com>
2015-07-01 12:03:25 -0700

    Allow user to add reviews to a game.

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,22 @@
+class ReviewsController < ApplicationController
+  def new
+    @game = Game.find(params[:game_id])
+    @review = @game.reviews.new
+  end
+
+  def create
+    @game = Game.find(params[:game_id])
+    @review = @game.reviews.new(review_params)
+    if @review.save
+      flash[:notice] = "New review has been added to " + @game.title + "!"
+      redirect_to game_path(@game)
+    else
+      render :new
+    end
+  end
+
+private
+  def review_params
+    params.require(:review).permit(:name, :opinion, :rating, :number)
+  end
+end
