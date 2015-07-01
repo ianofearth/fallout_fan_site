a3974366fe63a1841704e859de3d241500fd6d81
Ian Bevel & Jeff Lalic <student@epicodus.com>
2015-07-01 16:25:59 -0700

    add ability to edit and delete reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -20,6 +20,30 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def edit
+    @game = Game.find(params[:game_id])
+    @review = Review.find(params[:id])
+  end
+
+  def update
+    @game = Game.find(params[:game_id])
+    @review = Review.find(params[:id])
+    if @review.update(review_params)
+      flash[:notice] = @review.name + "'s review has been updated!"
+      redirect_to game_review_path(@game, @review)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @game = Game.find(params[:game_id])
+    @review = Review.find(params[:id])
+    @review.destroy
+    flash[:notice] = @review.name + "'s review has been deleted"
+    redirect_to game_path(@game)
+  end
+
 
 private
   def review_params
